bbdadab879f664593896e226f9fda9ff187afc68
Thomas <thomasrv543@gmail.com>
2026-05-31 02:26:05 -0500

    Fix static files config

--- desercion_escolar/settings.py
+++ desercion_escolar/settings.py
@@ -133,9 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
